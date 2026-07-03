5c2594ad849cb8987861ea011a5681149582f578
ViktorMallagant <75912306+ViktorMallagant@users.noreply.github.com>
2026-07-03 11:58:22 -0500

    Update EditorTab.vue

--- src/components/EditorTab.vue
+++ src/components/EditorTab.vue
@@ -572,7 +572,7 @@
         <v-col cols="12" v-if="store.allModifiers.length > 0 || store.entrepreneurSocialPenalties.length > 0">
           <v-card class="pa-4">
             <v-card-title class="text-uppercase text-caption font-weight-bold">
-              Modificateurs
+              Modifiers
             </v-card-title>
             <v-list density="compact">
               <v-list-item
