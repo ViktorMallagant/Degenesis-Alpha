1f872a68e02f4a4760d5193f3fe40b72761a0f5c
ViktorMallagant <75912306+ViktorMallagant@users.noreply.github.com>
2026-08-15 11:18:31 -0500

    Register Brenni translations

--- src/config/messages/index.ts
+++ src/config/messages/index.ts
@@ -8,6 +8,7 @@ import { clanNames } from "./clans/names";
 import { clanRanks } from "./clans/ranks";
 import { clanTemplateRanks } from "./clans/templateRanks";
 import { stukovMessages } from "./clans/stukov";
+import { brenniMessages } from "./clans/brenni";
 import { legacies } from "./legacies";
 
 export default {
@@ -15,30 +16,30 @@ export default {
     messages: messages.de,
     ...properties.de,
     culturesConceptsCults: culturesConceptsCults.de,
-    ranks: { ...ranks.de, ...clanRanks.de, ...clanTemplateRanks.de, ...stukovMessages.de.ranks },
+    ranks: { ...ranks.de, ...clanRanks.de, ...clanTemplateRanks.de, ...stukovMessages.de.ranks, ...brenniMessages.de.ranks },
     sheet: sheet.de,
-    potentials: { ...potentials.de, ...stukovMessages.de.potentials },
-    clans: { ...clanNames.de, ...stukovMessages.de.clans },
+    potentials: { ...potentials.de, ...stukovMessages.de.potentials, ...brenniMessages.de.potentials },
+    clans: { ...clanNames.de, ...stukovMessages.de.clans, ...brenniMessages.de.clans },
     legacies: legacies.de,
   },
   en: {
     messages: messages.en,
     ...properties.en,
     culturesConceptsCults: culturesConceptsCults.en,
-    ranks: { ...ranks.en, ...clanRanks.en, ...clanTemplateRanks.en, ...stukovMessages.en.ranks },
+    ranks: { ...ranks.en, ...clanRanks.en, ...clanTemplateRanks.en, ...stukovMessages.en.ranks, ...brenniMessages.en.ranks },
     sheet: sheet.en,
-    potentials: { ...potentials.en, ...stukovMessages.en.potentials },
-    clans: { ...clanNames.en, ...stukovMessages.en.clans },
+    potentials: { ...potentials.en, ...stukovMessages.en.potentials, ...brenniMessages.en.potentials },
+    clans: { ...clanNames.en, ...stukovMessages.en.clans, ...brenniMessages.en.clans },
     legacies: legacies.en,
   },
   fr: {
     messages: messages.fr,
     ...properties.fr,
     culturesConceptsCults: culturesConceptsCults.fr,
-    ranks: { ...ranks.fr, ...clanRanks.fr, ...clanTemplateRanks.fr, ...stukovMessages.fr.ranks },
+    ranks: { ...ranks.fr, ...clanRanks.fr, ...clanTemplateRanks.fr, ...stukovMessages.fr.ranks, ...brenniMessages.fr.ranks },
     sheet: sheet.fr,
-    potentials: { ...potentials.fr, ...stukovMessages.fr.potentials },
-    clans: { ...clanNames.fr, ...stukovMessages.fr.clans },
+    potentials: { ...potentials.fr, ...stukovMessages.fr.potentials, ...brenniMessages.fr.potentials },
+    clans: { ...clanNames.fr, ...stukovMessages.fr.clans, ...brenniMessages.fr.clans },
     legacies: legacies.fr
   }
 }
