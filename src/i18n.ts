19dedb68cc3f9036b3dde2ff2ae01de000ee1a72
ViktorMallagant <75912306+ViktorMallagant@users.noreply.github.com>
2026-08-14 19:01:36 -0500

    Default new visitors to English

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,16 @@
 import { createI18n } from 'vue-i18n'
 import messages from './config/messages'
 
+const storedLocale = localStorage.getItem('locale')
+const initialLocale = storedLocale || 'en'
+
+if (!storedLocale) {
+  localStorage.setItem('locale', 'en')
+}
+
 export const i18n = createI18n({
   legacy: false,
-  locale: 'fr',
+  locale: initialLocale,
   fallbackWarn: false,
   messages
 })
