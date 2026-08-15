a30e90e93ea8b3475c14041ff2f2286d2cce7ed8
ViktorMallagant <75912306+ViktorMallagant@users.noreply.github.com>
2026-08-14 22:57:29 -0500

    Add Hunter Gatherer template clan batch

---
+++ src/config/cults/clanners/clans/hunterGathererTemplateClans.ts
@@ -0,0 +1,48 @@
+import { createHunterGathererClan } from './hunterGathererTemplate'
+
+const mechansTemplate = createHunterGathererClan('mechans')
+const phosphoritesTemplate = createHunterGathererClan('phosphorites')
+const enemoiTemplate = createHunterGathererClan('enemoi')
+const stukovNomadsTemplate = createHunterGathererClan('stukovNomads')
+const storskisTemplate = createHunterGathererClan('storskis')
+const corpseEatersTemplate = createHunterGathererClan('corpseEaters')
+const gargantiTemplate = createHunterGathererClan('garganti')
+const voivodulesTemplate = createHunterGathererClan('voivodules')
+const matadorsTemplate = createHunterGathererClan('matadors')
+const flayersTemplate = createHunterGathererClan('flayers')
+const adrianiTemplate = createHunterGathererClan('adriani')
+const romanosTemplate = createHunterGathererClan('romanos')
+const masaiTemplate = createHunterGathererClan('masai')
+const shabathTemplate = createHunterGathererClan('shabath')
+
+export const Mechans = mechansTemplate.clan
+export const Phosphorites = phosphoritesTemplate.clan
+export const Enemoi = enemoiTemplate.clan
+export const StukovNomads = stukovNomadsTemplate.clan
+export const Storskis = storskisTemplate.clan
+export const CorpseEaters = corpseEatersTemplate.clan
+export const Garganti = gargantiTemplate.clan
+export const Voivodules = voivodulesTemplate.clan
+export const Matadors = matadorsTemplate.clan
+export const Flayers = flayersTemplate.clan
+export const Adriani = adrianiTemplate.clan
+export const Romanos = romanosTemplate.clan
+export const Masai = masaiTemplate.clan
+export const Shabath = shabathTemplate.clan
+
+export const HunterGathererTemplateClanRanks = [
+  ...mechansTemplate.ranks,
+  ...phosphoritesTemplate.ranks,
+  ...enemoiTemplate.ranks,
+  ...stukovNomadsTemplate.ranks,
+  ...storskisTemplate.ranks,
+  ...corpseEatersTemplate.ranks,
+  ...gargantiTemplate.ranks,
+  ...voivodulesTemplate.ranks,
+  ...matadorsTemplate.ranks,
+  ...flayersTemplate.ranks,
+  ...adrianiTemplate.ranks,
+  ...romanosTemplate.ranks,
+  ...masaiTemplate.ranks,
+  ...shabathTemplate.ranks
+]
