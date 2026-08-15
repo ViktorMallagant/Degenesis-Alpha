4a4c7e566cd8c14d5c073e80c00065b1af7df084
ViktorMallagant <75912306+ViktorMallagant@users.noreply.github.com>
2026-08-15 01:17:38 -0500

    Merge Stukov translations

--- src/config/messages/index.ts
+++ src/config/messages/index.ts
@@ -7,6 +7,7 @@ import { potentials } from "./potentials";
 import { clanNames } from "./clans/names";
 import { clanRanks } from "./clans/ranks";
 import { clanTemplateRanks } from "./clans/templateRanks";
+import { stukovMessages } from "./clans/stukov";
 import { legacies } from "./legacies";
 
 export default {
@@ -14,30 +15,30 @@ export default {
     messages: messages.de,
     ...properties.de,
     culturesConceptsCults: culturesConceptsCults.de,
-    ranks: { ...ranks.de, ...clanRanks.de, ...clanTemplateRanks.de },
+    ranks: { ...ranks.de, ...clanRanks.de, ...clanTemplateRanks.de, ...stukovMessages.de.ranks },
     sheet: sheet.de,
-    potentials: potentials.de,
-    clans: clanNames.de,
+    potentials: { ...potentials.de, ...stukovMessages.de.potentials },
+    clans: { ...clanNames.de, ...stukovMessages.de.clans },
     legacies: legacies.de,
   },
   en: {
     messages: messages.en,
     ...properties.en,
     culturesConceptsCults: culturesConceptsCults.en,
-    ranks: { ...ranks.en, ...clanRanks.en, ...clanTemplateRanks.en },
+    ranks: { ...ranks.en, ...clanRanks.en, ...clanTemplateRanks.en, ...stukovMessages.en.ranks },
     sheet: sheet.en,
-    potentials: potentials.en,
-    clans: clanNames.en,
+    potentials: { ...potentials.en, ...stukovMessages.en.potentials },
+    clans: { ...clanNames.en, ...stukovMessages.en.clans },
     legacies: legacies.en,
   },
   fr: {
     messages: messages.fr,
     ...properties.fr,
     culturesConceptsCults: culturesConceptsCults.fr,
-    ranks: { ...ranks.fr, ...clanRanks.fr, ...clanTemplateRanks.fr },
+    ranks: { ...ranks.fr, ...clanRanks.fr, ...clanTemplateRanks.fr, ...stukovMessages.fr.ranks },
     sheet: sheet.fr,
-    potentials: potentials.fr,
-    clans: clanNames.fr,
+    potentials: { ...potentials.fr, ...stukovMessages.fr.potentials },
+    clans: { ...clanNames.fr, ...stukovMessages.fr.clans },
     legacies: legacies.fr
   }
 }
