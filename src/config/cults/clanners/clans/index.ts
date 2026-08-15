cf613974f5cd974f6eae16d5bea6d6b900614627
ViktorMallagant <75912306+ViktorMallagant@users.noreply.github.com>
2026-08-14 22:57:36 -0500

    Register Hunter Gatherer template clans

--- src/config/cults/clanners/clans/index.ts
+++ src/config/cults/clanners/clans/index.ts
@@ -2,6 +2,23 @@ import { Bordenoir, BordenoirRanks } from "./bordenoir";
 import { Cockroaches, CockroachRanks } from "./cockroaches";
 import { Exalters, ExaltersRanks } from "./exalters";
 import { HunterGathererRanks, HunterGatherers } from "./hunterGatherers";
+import {
+  Adriani,
+  CorpseEaters,
+  Enemoi,
+  Flayers,
+  Garganti,
+  HunterGathererTemplateClanRanks,
+  Masai,
+  Matadors,
+  Mechans,
+  Phosphorites,
+  Romanos,
+  Shabath,
+  Storskis,
+  StukovNomads,
+  Voivodules
+} from "./hunterGathererTemplateClans";
 import { Pneumancers, PneumancersRanks } from "./pneumancers";
 import { Resistance, ResistanceRanks } from "./resistance";
 import { Sanglier, SanglierRanks } from "./sanglier";
@@ -10,6 +27,20 @@ import { Touloni, TouloniRanks } from "./touloni";
 export const Clans = {
   HunterGatherers: HunterGatherers,
   Cockroaches: Cockroaches,
+  Mechans: Mechans,
+  Phosphorites: Phosphorites,
+  Enemoi: Enemoi,
+  StukovNomads: StukovNomads,
+  Storskis: Storskis,
+  CorpseEaters: CorpseEaters,
+  Garganti: Garganti,
+  Voivodules: Voivodules,
+  Matadors: Matadors,
+  Flayers: Flayers,
+  Adriani: Adriani,
+  Romanos: Romanos,
+  Masai: Masai,
+  Shabath: Shabath,
   Touloni: Touloni,
   Sanglier: Sanglier,
   Bordenoir: Bordenoir,
@@ -21,6 +52,7 @@ export const Clans = {
 export const ClannerRanks = [
   ...HunterGathererRanks,
   ...CockroachRanks,
+  ...HunterGathererTemplateClanRanks,
   ...TouloniRanks,
   ...SanglierRanks,
   ...BordenoirRanks,
