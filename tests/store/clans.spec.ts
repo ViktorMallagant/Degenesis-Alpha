1cee18da5c157b188ef97f7f73bf6c86876288fa
ViktorMallagant <75912306+ViktorMallagant@users.noreply.github.com>
2026-08-14 22:58:48 -0500

    Test Hunter Gatherer template clan batch

--- tests/store/clans.spec.ts
+++ tests/store/clans.spec.ts
@@ -10,84 +10,94 @@ beforeEach(() => {
 })
 
 test('Setting the cult to Clanners selects HunterGatherers as the clan', () => {
-    const store = useCharacterStore()
+  const store = useCharacterStore()
 
-    // Given the cult is not set to Clanners
-    expect(store.clan).toBeUndefined()
-
-    // When the cult is set to Clanners
-    store.setCult(config.cults.Clanners)
-
-    // Then the clan is set to HunterGatherers
-    expect(store.clan).toEqual(config.clans.HunterGatherers)
+  expect(store.clan).toBeUndefined()
+  store.setCult(config.cults.Clanners)
+  expect(store.clan).toEqual(config.clans.HunterGatherers)
 })
 
 test('Switching the cult away from Clanners sets the clan to undefined', () => {
-    const store = useCharacterStore()
-
-    // Given the cult is set to Clanners
-    store.setCult(config.cults.Clanners)
-    expect(store.clan).toEqual(config.clans.HunterGatherers)
-    
-    // When the cult is set to something else
-    store.setCult(config.cults.Spitalians)
-
-    // Then the clan is set to undefined
-    expect(store.clan).toBeUndefined()
-})
-
-test('Setting the clan is not possible if the cult is not set to Clanners', () => {
-    const store = useCharacterStore()
-
-    // Given the cult is not set to Clanners
-    store.setCult(config.cults.Spitalians)
-    expect(store.clan).toBeUndefined()
-    
-    // When the clan is set
-    store.setClan(config.clans.HunterGatherers)
-
-    // Then the clan is not set 
-    expect(store.clan).toBeUndefined()
-})
-
-test('Setting the clan provides the skill bonuses of the selected clan', () => {
-    const store = useCharacterStore()
-
-    // Given the cult set to Clanners (and the clan set to HunterGatherers)
-    store.setCult(config.cults.Clanners)
-    store.setClan(config.clans.HunterGatherers)
-    
-    // Then the skill bonuses for HunterGatherers are provided
-    expect(store.skillMax(Skills.stamina)).toEqual(3)
-})
-
-test('Cockroaches is a selectable Hunter Gatherer template clan', () => {
   const store = useCharacterStore()
 
   store.setCult(config.cults.Clanners)
-  store.setClan(config.clans.Cockroaches)
+  expect(store.clan).toEqual(config.clans.HunterGatherers)
 
-  expect(store.clan).toEqual(config.clans.Cockroaches)
-  expect(store.clan?.name).toBe('cockroaches')
-  expect(store.skillMax(Skills.stamina)).toEqual(3)
-  expect(store.skillMax(Skills.melee)).toEqual(3)
-  expect(store.skillMax(Skills.survival)).toEqual(3)
-  expect(store.skillMax(Skills.legends)).toEqual(3)
-  expect(store.skillMax(Skills.taming)).toEqual(3)
+  store.setCult(config.cults.Spitalians)
+  expect(store.clan).toBeUndefined()
 })
 
-test('Cockroaches inherits the Hunter Gatherer rank tree', () => {
-  const ranks = ranksByCult(config.cults.Clanners, config.clans.Cockroaches)
+test('Setting the clan is not possible if the cult is not set to Clanners', () => {
+  const store = useCharacterStore()
 
-  expect(ranks.map((rank) => rank.name)).toEqual([
-    'cockroaches-scout',
-    'cockroaches-hunter',
-    'cockroaches-gatherer',
-    'cockroaches-tribalWarrior',
-    'cockroaches-shaman',
-    'cockroaches-chieftain',
-    'cockroaches-champion',
-    'cockroaches-founder'
-  ])
-  expect(ranks.every((rank) => rank.clan?.name === 'cockroaches')).toBe(true)
+  store.setCult(config.cults.Spitalians)
+  expect(store.clan).toBeUndefined()
+
+  store.setClan(config.clans.HunterGatherers)
+  expect(store.clan).toBeUndefined()
+})
+
+test('Setting the clan provides the skill bonuses of the selected clan', () => {
+  const store = useCharacterStore()
+
+  store.setCult(config.cults.Clanners)
+  store.setClan(config.clans.HunterGatherers)
+
+  expect(store.skillMax(Skills.stamina)).toEqual(3)
+})
+
+const hunterGathererTemplateClans = [
+  config.clans.Cockroaches,
+  config.clans.Mechans,
+  config.clans.Phosphorites,
+  config.clans.Enemoi,
+  config.clans.StukovNomads,
+  config.clans.Storskis,
+  config.clans.CorpseEaters,
+  config.clans.Garganti,
+  config.clans.Voivodules,
+  config.clans.Matadors,
+  config.clans.Flayers,
+  config.clans.Adriani,
+  config.clans.Romanos,
+  config.clans.Masai,
+  config.clans.Shabath
+]
+
+const expectedRankSuffixes = [
+  'scout',
+  'hunter',
+  'gatherer',
+  'tribalWarrior',
+  'shaman',
+  'chieftain',
+  'champion',
+  'founder'
+]
+
+test('Hunter Gatherer template clans are selectable and inherit standard bonuses', () => {
+  const store = useCharacterStore()
+  store.setCult(config.cults.Clanners)
+
+  for (const clan of hunterGathererTemplateClans) {
+    store.setClan(clan)
+
+    expect(store.clan).toEqual(clan)
+    expect(store.skillMax(Skills.stamina)).toEqual(3)
+    expect(store.skillMax(Skills.melee)).toEqual(3)
+    expect(store.skillMax(Skills.survival)).toEqual(3)
+    expect(store.skillMax(Skills.legends)).toEqual(3)
+    expect(store.skillMax(Skills.taming)).toEqual(3)
+  }
+})
+
+test('Hunter Gatherer template clans inherit the standard eight-rank tree', () => {
+  for (const clan of hunterGathererTemplateClans) {
+    const ranks = ranksByCult(config.cults.Clanners, clan)
+
+    expect(ranks.map((rank) => rank.name)).toEqual(
+      expectedRankSuffixes.map((suffix) => `${clan.name}-${suffix}`)
+    )
+    expect(ranks.every((rank) => rank.clan?.name === clan.name)).toBe(true)
+  }
 })
