553fec98c7cf6955e406161fb053d2c2f19d40b5
ViktorMallagant <75912306+ViktorMallagant@users.noreply.github.com>
2026-08-16 16:22:38 -0500

    Use culture currency on character sheet

--- src/components/SheetText.vue
+++ src/components/SheetText.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="wrapper">
-    <span v-if="title.length > 0" class="text-uppercase">{{ title }}: </span>
+    <span v-if="displayTitle.length > 0" class="text-uppercase">{{ displayTitle }}: </span>
     <span>{{ text }}</span>
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useCharacterStore } from '@/store'
+
 export interface Props {
   title?: string
   text?: string
 }
-withDefaults(defineProps<Props>(), {
+
+const props = withDefaults(defineProps<Props>(), {
   title: '',
   text: ''
+})
+
+const store = useCharacterStore()
+const i18n = useI18n()
+
+const displayTitle = computed(() => {
+  if (i18n.locale.value === 'en' && props.title === i18n.t('messages.dinars')) {
+    return store.culture?.name === 'africa' ? 'Dinars' : 'Drafts'
+  }
+  return props.title
 })
 </script>
 
