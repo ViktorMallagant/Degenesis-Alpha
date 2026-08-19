64944c66913a2f975f76365ad4eb1dfe2802d5b6
ViktorMallagant <75912306+ViktorMallagant@users.noreply.github.com>
2026-08-19 14:02:56 -0500

    Use consolidated clan message catalog

--- src/config/messages/index.ts
+++ src/config/messages/index.ts
@@ -4,17 +4,7 @@ import { sheet } from "./sheet";
 import { properties } from "./properties";
 import { ranks } from "./ranks";
 import { potentials } from "./potentials";
-import { clanNames } from "./clans/names";
-import { clanRanks } from "./clans/ranks";
-import { clanTemplateRanks } from "./clans/templateRanks";
-import { stukovMessages } from "./clans/stukov";
-import { brenniMessages } from "./clans/brenni";
-import { providersMessages } from "./clans/providers";
-import { steelMastersMessages } from "./clans/steelmasters";
-import { britoniMessages } from "./clans/britoni";
-import { pictonsMessages } from "./clans/pictons";
-import { druidsMessages } from "./clans/druids";
-import { ganaridsMessages } from "./clans/ganarids";
+import { clanMessages } from "./clans/catalog";
 import { legacies } from "./legacies";
 
 // Normalize user-facing English description markup and terminology in one place.
@@ -58,30 +48,30 @@ export default {
     messages: { ...messages.de, missingConditions: 'Fehlende Voraussetzungen' },
     ...properties.de,
     culturesConceptsCults: culturesConceptsCults.de,
-    ranks: { ...ranks.de, ...clanRanks.de, ...clanTemplateRanks.de, ...stukovMessages.de.ranks, ...brenniMessages.de.ranks, ...providersMessages.de.ranks, ...steelMastersMessages.de.ranks, ...britoniMessages.de.ranks, ...pictonsMessages.de.ranks, ...druidsMessages.de.ranks, ...ganaridsMessages.de.ranks },
+    ranks: { ...ranks.de, ...clanMessages.de.ranks },
     sheet: sheet.de,
-    potentials: { ...potentials.de, ...stukovMessages.de.potentials, ...brenniMessages.de.potentials, ...providersMessages.de.potentials, ...steelMastersMessages.de.potentials, ...britoniMessages.de.potentials, ...pictonsMessages.de.potentials, ...druidsMessages.de.potentials, ...ganaridsMessages.de.potentials },
-    clans: { ...clanNames.de, ...stukovMessages.de.clans, ...brenniMessages.de.clans, ...providersMessages.de.clans, ...steelMastersMessages.de.clans, ...britoniMessages.de.clans, ...pictonsMessages.de.clans, ...druidsMessages.de.clans, ...ganaridsMessages.de.clans },
+    potentials: { ...potentials.de, ...clanMessages.de.potentials },
+    clans: clanMessages.de.clans,
     legacies: legacies.de,
   },
   en: {
     messages: normalizeEnglishText({ ...messages.en, missingConditions: 'Missing prerequisites' }),
     ...normalizeEnglishText(properties.en),
     culturesConceptsCults: normalizeEnglishText(culturesConceptsCults.en),
-    ranks: normalizeEnglishText({ ...ranks.en, ...clanRanks.en, ...clanTemplateRanks.en, ...stukovMessages.en.ranks, ...brenniMessages.en.ranks, ...providersMessages.en.ranks, ...steelMastersMessages.en.ranks, ...britoniMessages.en.ranks, ...pictonsMessages.en.ranks, ...druidsMessages.en.ranks, ...ganaridsMessages.en.ranks }),
+    ranks: normalizeEnglishText({ ...ranks.en, ...clanMessages.en.ranks }),
     sheet: normalizeEnglishText(sheet.en),
-    potentials: normalizeEnglishText({ ...potentials.en, ...stukovMessages.en.potentials, ...brenniMessages.en.potentials, ...providersMessages.en.potentials, ...steelMastersMessages.en.potentials, ...britoniMessages.en.potentials, ...pictonsMessages.en.potentials, ...druidsMessages.en.potentials, ...ganaridsMessages.en.potentials }),
-    clans: normalizeEnglishText({ ...clanNames.en, ...stukovMessages.en.clans, ...brenniMessages.en.clans, ...providersMessages.en.clans, ...steelMastersMessages.en.clans, ...britoniMessages.en.clans, ...pictonsMessages.en.clans, ...druidsMessages.en.clans, ...ganaridsMessages.en.clans }),
+    potentials: normalizeEnglishText({ ...potentials.en, ...clanMessages.en.potentials }),
+    clans: normalizeEnglishText(clanMessages.en.clans),
     legacies: normalizeEnglishText(legacies.en),
   },
   fr: {
     messages: { ...messages.fr, missingConditions: 'Prérequis manquants' },
     ...properties.fr,
     culturesConceptsCults: culturesConceptsCults.fr,
-    ranks: { ...ranks.fr, ...clanRanks.fr, ...clanTemplateRanks.fr, ...stukovMessages.fr.ranks, ...brenniMessages.fr.ranks, ...providersMessages.fr.ranks, ...steelMastersMessages.fr.ranks, ...britoniMessages.fr.ranks, ...pictonsMessages.fr.ranks, ...druidsMessages.fr.ranks, ...ganaridsMessages.fr.ranks },
+    ranks: { ...ranks.fr, ...clanMessages.fr.ranks },
     sheet: sheet.fr,
-    potentials: { ...potentials.fr, ...stukovMessages.fr.potentials, ...brenniMessages.fr.potentials, ...providersMessages.fr.potentials, ...steelMastersMessages.fr.potentials, ...britoniMessages.fr.potentials, ...pictonsMessages.fr.potentials, ...druidsMessages.fr.potentials, ...ganaridsMessages.fr.potentials },
-    clans: { ...clanNames.fr, ...stukovMessages.fr.clans, ...brenniMessages.fr.clans, ...providersMessages.fr.clans, ...steelMastersMessages.fr.clans, ...britoniMessages.fr.clans, ...pictonsMessages.fr.clans, ...druidsMessages.fr.clans, ...ganaridsMessages.fr.clans },
+    potentials: { ...potentials.fr, ...clanMessages.fr.potentials },
+    clans: clanMessages.fr.clans,
     legacies: legacies.fr
   }
 }
